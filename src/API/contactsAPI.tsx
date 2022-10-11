26add7c4a45d444ed46c0e2401f51a579cd375c4
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-10-11 10:00:29 +0300

    .

--- src/API/contactsAPI.tsx
+++ src/API/contactsAPI.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 axios.defaults.baseURL = 'https://62f25c79b1098f1508112d73.mockapi.io';
 
-export const getContacts = (): Promise<AxiosResponse<{ id: string, name:string, phone:string }[]>> => {
+export const getContacts = (): Promise<AxiosResponse<{ id: string, name:string, number:string }[]>> => {
   return axios.get('/contacts');
 };
 
